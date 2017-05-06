9f20716a4509fe2c6e70965cefda3792cf7b4f49
Alexander Miertsch <kontakt@codeliner.ws>
2017-05-06 23:10:24 +0200

    Empty user AR

--- src/Model/User.php
+++ src/Model/User.php
@@ -11,44 +11,10 @@ use Prooph\Workshop\Model\User\UserId;
 
 final class User extends AggregateRoot
 {
-    /**
-     * @var UserId
-     */
-    private $id;
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var string
-     */
-    private $email;
-
-    public static function register(UserId $userId, string $name, string $email): User
-    {
-        $self = new self();
-
-        $self->recordThat(UserWasRegistered::occur($userId->toString(), [
-            'name' => $name,
-            'email' => $email
-        ]));
-
-        return $self;
-    }
-
-    public function changeName(string $newName): void
-    {
-        $this->recordThat(UsernameWasChanged::occur($this->id->toString(), [
-            'oldName' => $this->name,
-            'newName' => $newName
-        ]));
-    }
 
     protected function aggregateId(): string
     {
-        return $this->id->toString();
+        // TODO: Implement aggregateId() method.
     }
 
     /**
@@ -56,20 +22,6 @@ final class User extends AggregateRoot
      */
     protected function apply(AggregateChanged $event): void
     {
-        $method = 'on' . $event->messageName();
-
-        $this->{$method}($event);
-    }
-
-    private function onUserWasRegistered(UserWasRegistered $event): void
-    {
-        $this->id = $event->userId();
-        $this->name = $event->name();
-        $this->email = $event->email();
-    }
-
-    private function onUsernameWasChanged(UsernameWasChanged $event): void
-    {
-        $this->name = $event->newName();
+        // TODO: Implement apply() method.
     }
 }
